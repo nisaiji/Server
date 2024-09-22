515e4745d602fc85dd4579bd336a6c23feae1f06
Kuldeep Panwar <kuldeeppanwar460@gmail.com>
2024-09-22 13:18:39 +0530

    changes in authorizations

--- src/routers/section.router.js
+++ src/routers/section.router.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {deleteSectionController,getSectionController,registerSectionController, replaceTeacherController,} from "../controllers/section.controller.js";
 import { adminAuthenticate } from "../middlewares/authentication/admin.authentication.middleware.js";
-import { deleteSectionAuthorization } from "../middlewares/authorization/deleteSection.authorization.middleware.js";
 import { registerSectionValidation } from "../middlewares/validation/section.validation.middleware.js";
 
 const sectionRouter = express.Router();
@@ -9,6 +8,6 @@ const sectionRouter = express.Router();
 sectionRouter.post("/", adminAuthenticate, registerSectionValidation, registerSectionController);
 sectionRouter.put("/replace-teacher",adminAuthenticate,replaceTeacherController);
 sectionRouter.get("/:sectionId", adminAuthenticate, getSectionController);
-sectionRouter.delete("/:sectionId", adminAuthenticate,deleteSectionAuthorization,deleteSectionController);
+sectionRouter.delete("/:sectionId", adminAuthenticate, deleteSectionController);
 
 export default sectionRouter; 
