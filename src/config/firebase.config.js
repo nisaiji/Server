2f3791d6bbbb88601b3c8cff694a0499b4d96ad9
Kuldeep Panwar <kuldeeppanwar460@gmail.com>
2025-06-14 19:08:04 +0530

    temp

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,7 +2,6 @@ import admin from "firebase-admin";
 // import serviceAccount from "../../sharedri-firebase-adminsdk-fbsvc-8f948b3654.json" assert { type: "json" };
 import serviceAccount from "../../nikhilesh-firebase.json" assert { type: "json" };
 
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
