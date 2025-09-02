c33e21b042dc349364c30403d6bde7ab38e3b234
Kuldeep Panwar <kuldeeppanwar460@gmail.com>
2025-09-02 21:56:34 +0530

    get teachers of section

--- src/routers/teacherSubjectSection.router.js
+++ src/routers/teacherSubjectSection.router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { adminAuthenticate } from "../middlewares/authentication/admin.authentication.middleware.js";
-import { createTeacherSubjectSectionController, getAllSubjectsOfTeacherInSectionController } from "../controllers/teacherSubjectSection.controller.js";
+import { createTeacherSubjectSectionController, getAllSubjectsOfTeacherInSectionController, getAllSubjectsTeachersOfSectionController } from "../controllers/teacherSubjectSection.controller.js";
 import { teacherAuthenticate } from "../middlewares/authentication/teacher.authentication.middleware.js";
 
 const teacherSubjectSectionRouter = express.Router();
@@ -8,6 +8,7 @@ const teacherSubjectSectionRouter = express.Router();
 teacherSubjectSectionRouter.post("/", adminAuthenticate, createTeacherSubjectSectionController );
 teacherSubjectSectionRouter.post("/", teacherAuthenticate, createTeacherSubjectSectionController );
 teacherSubjectSectionRouter.get("/teacher", teacherAuthenticate, getAllSubjectsOfTeacherInSectionController);
+teacherSubjectSectionRouter.get("/class-teacher", teacherAuthenticate, getAllSubjectsTeachersOfSectionController);
 
 
 export default teacherSubjectSectionRouter;
