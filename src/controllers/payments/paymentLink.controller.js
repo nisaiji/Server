3e6d64fe4609e4137deb5430c17b2857a64b9737
Kuldeep Panwar <kuldeeppanwar460@gmail.com>
2025-12-16 22:47:53 +0530

    feat: setup wiston logs

--- src/controllers/payments/paymentLink.controller.js
+++ src/controllers/payments/paymentLink.controller.js
@@ -9,17 +9,17 @@ import { getStudentService } from "../../services/student.service.js";
 import { getAdminService } from "../../services/admin.services.js";
 import { createPaymentTransactionService, getPaymentTransactionService } from "../../services/paymentTransaction.service.js";
 import { getParentService } from "../../services/v2/parent.services.js";
-import { get } from "mongoose";
+import logger from "../../logger/index.js";
 
 export async function createPaymentLinkController(req, res) {
   try {
     const { amount, sessionStudentId, description } = req.body;
+    logger.info("Creating payment link", { requestBody: req.body });
     const parentId = req.parentId;
     const sessionStudent = await getSessionStudentService({_id: sessionStudentId});
     if(!sessionStudent) {
       return res.status(StatusCodes.BAD_REQUEST).send(error(400, "Student not found"));
     }
-
 
     let [school, marchant, student, parent] = await Promise.all([
       getAdminService({_id: sessionStudent['school']}),
@@ -52,7 +52,7 @@ export async function createPaymentLinkController(req, res) {
           zohoAccessToken: response.access_token,
         }
       );
-
+      logger.info("Zoho access token refreshed", { marchantId: marchant._id, expiresAt });
       marchant = await getMarchantPaymentConfigService({ _id: marchant._id });
     }
     const linkExpiryDate = new Date();
@@ -76,12 +76,13 @@ export async function createPaymentLinkController(req, res) {
       phone: parent.phone,
       email: parent.email,
       referenceNumber: generateReferenceNumber({sessionStudentId: sessionStudent['_id']}),
-      expiresAt: linkExpiryDate,
+      // expiresAt: linkExpiryDate,
       notifyUser: true,
       returnUrl: config.zohoRedirectUrl
     })
     return res.status(StatusCodes.OK).send(success(200, paymentLinkResponse));
   } catch (err) {
+    logger.error("Error creating payment link", {}, err);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error(500, err.message));
   }
 }
