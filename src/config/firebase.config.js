13bace2fedf28f12e5c14e6c50127659a9149c6e
Uday Singh <reachudaysingh@gmail.com>
2026-01-02 20:20:39 +0530

    Added Redis Setup

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../firebase.json" with { type: "json" };
+import serviceAccount from "../../firebase.json" assert { type: "json" };
 
 const imageUrl = "http://localhost:4000/images/logo.jpeg";
 admin.initializeApp({
@@ -8,24 +8,24 @@ admin.initializeApp({
 
 export async function sendPushNotification(fcmToken, title, body) {
   try {
-  if(!fcmToken) {
-    return;
-  }
-  const message = {
-    notification: {
-      title,
-      body,
-      image: imageUrl
-    },
-    token: fcmToken,
-  };
-  const response = await admin.messaging().send(message);
-  return response;
+    if (!fcmToken) {
+      return;
+    }
+    const message = {
+      notification: {
+        title,
+        body,
+        image: imageUrl,
+      },
+      token: fcmToken,
+    };
+    const response = await admin.messaging().send(message);
+    return response;
   } catch (error) {
-    console.log({error})
-    if (error.code === 'messaging/registration-token-not-registered') {
-        console.log('Invalid token detected. Removing from DB:');
-        // await deleteTokenFromDB(token);
+    console.log({ error });
+    if (error.code === "messaging/registration-token-not-registered") {
+      console.log("Invalid token detected. Removing from DB:");
+      // await deleteTokenFromDB(token);
     }
   }
 }
